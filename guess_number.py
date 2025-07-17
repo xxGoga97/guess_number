d3a42fb316381b1b48a701c852214919314fe18c
Vlad Dmitriev xxGoga <xxgoga97@gmail.com>
2025-07-17 10:59:07 +0300

    asd

--- guess_number.py
+++ guess_number.py
@@ -6,6 +6,7 @@ from random import randint
 number = randint(1, 100)
 print('Угадайте число от 1 до 100')
 
+
 while True:
     # Получаем число от пользователя и сохраняем его в переменную.
     guess = int(input('Введите число: '))
